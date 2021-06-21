bdc18ba9549d36383b9a5a29c8ee91f900631881
carlos <mtcarlos98@gmail.com>
2021-06-21 15:16:11 +0200

    Project 2 (code) finished

--- Project2/Project2_fun.py
+++ Project2/Project2_fun.py
@@ -11,21 +11,35 @@ from scipy import stats
 from time import time
 from numpy.random import *
 
-def beds_simulation(realoc_probs, params, n_patients=5000):
+
+def ward_sample(size, scale, distr_type):
+    if distr_type == "exp":
+        out = exponential(size=size, scale = scale)
+    elif distr_type == "lognormal":
+        out = lognormal(sigma=4*scale**2, size=size)
+    else:
+        raise NotImplementedError("Not implemented distribution type: choose exp or lognormal")
+    return out
+        
+
+def beds_simulation(realoc_probs, params, urgency_weight = False, ward_distribution = "exp", n_patients=5000):
     if realoc_probs.shape[0] != params.shape[0]:
-        raise SyntaxError("Dimensions of parameters and realocation probabilites do not match!")
+        raise SyntaxError("Dimensions of parameters and realocation probabilites do not match!")        
         
     n_types = params.shape[0]
     bed_capacities = params[:,0]
     arrival_rates = params[:,1]
     ward_rates = params[:,2]
-    urgency_pts = params[:,3]
+    urgency_pts = params[:,3]    
     
     ## Build arrival times
     arrival_time_dist = np.zeros(n_patients)
     arrival_types = np.zeros(n_patients) ## To store the type of the patient and assign it to a ward
+    patient_types_count = np.zeros(n_types) ## store total number of patients in each category
     for i in range(n_patients):
         patient_type = randint(0,n_types)
+        patient_types_count[patient_type] +=1
+        
         arrival_time_dist[i] = exponential(size=1, scale=1/arrival_rates[patient_type])
         arrival_types[i] = patient_type
     arrival_times = np.cumsum(arrival_time_dist)
@@ -33,7 +47,8 @@ def beds_simulation(realoc_probs, params, n_patients=5000):
     ## Build ward service times
     ward_time_dist = np.zeros((n_types, n_patients))
     for i in range(n_types):
-        ward_time_dist[i] = exponential(size=[n_patients], scale = ward_rates[i])
+        #ward_time_dist[i] = exponential(size=[n_patients], scale = ward_rates[i])
+        ward_time_dist[i] = ward_sample(distr_type=ward_distribution, size=n_patients, scale=ward_rates[i])
     
     ## Start counter of beds occupation for each ward
     beds_occupied = np.zeros(n_types)
@@ -41,39 +56,49 @@ def beds_simulation(realoc_probs, params, n_patients=5000):
     ward_times = np.zeros(n_types)
     
     ## Start counter for rejected patients
-    rejected = 0
+    rejected = np.zeros(n_types)
+    ## Start counter for relocated patients
+    relocated = np.zeros(n_types)
+    ## Start counter for patients alocated in their correct ward
+    accepted = np.zeros(n_types)
+    
+    ## Count total penalty (we try to minimize)
+    penalty = 0
     
     for n in range(n_patients):
-        n_patient_type = int(arrival_types[n])
+        n_patient_type = int(arrival_types[n]) ## keep track of the patient type in this iteration
         just_freed_beds = np.where(ward_times<arrival_times[n])[0] ## Check beds that has just been freed
-        # print("#######")
-        # print(bed_capacities)
-        # print(beds_occupied)
 
         if just_freed_beds.shape[0] != 0:
             beds_occupied[just_freed_beds] -= 1 ## Remove patient from the ward
             beds_occupied[beds_occupied<0] = 0 ## To avoid negative occupation values
             
         if (beds_occupied[n_patient_type] < bed_capacities[n_patient_type]):
+            accepted[n_patient_type] += 1
             beds_occupied[n_patient_type] += 1
             ward_times[n_patient_type] = ward_time_dist[n_patient_type,n] + arrival_times[n]
             
         elif np.sum(beds_occupied<bed_capacities)>0: ## Check if there is any other ward with beds available
+            relocated[n_patient_type] += 1
+            penalty += urgency_pts[n_patient_type]
             wards_available = np.where(beds_occupied<bed_capacities)[0]
             probs = realoc_probs[n_patient_type,wards_available]
             
-            if probs[0] == 0:
-                rejected += 1
+            if np.sum(probs) == 0:
+                rejected[n_patient_type] += 1
             else:
                 probs_normalized = probs/np.sum(probs) ## Normalize the probabilities to sample from them
+                if urgency_weight:
+                    probs_normalized[:] = 0
+                    min_urgency = np.where(urgency_pts[wards_available] == np.min(urgency_pts[wards_available]))[0]
+                    probs_normalized[min_urgency] = 1
+                    probs_normalized = probs_normalized/np.sum(probs_normalized)
+                
                 select_ward = choice(wards_available, p=probs_normalized) ## Choose one of the other available wards
-                # print(n_patient_type)
-                # print("asdfasdf")
                 beds_occupied[select_ward] += 1
                 ward_times[select_ward] = ward_time_dist[n_patient_type,n] + arrival_times[n]
-                # print(beds_occupied)
             
         else: ## No beds available
-            rejected += 1
+            rejected[n_patient_type] += 1
             
-    return rejected
+    return accepted, relocated, rejected, patient_types_count, penalty
--- Project2/Project2_main.py
+++ Project2/Project2_main.py
@@ -12,20 +12,50 @@ from time import time
 from numpy.random import *
 from Project2_fun import * 
 
-#%%
-realocation_probs = np.array([[0,0.05,0.1,0.05,0.8], [0.2,0,0.5,0.15,0.15], [0.3,0.2,0,0.2,0.3], [0.35,0.3,0.05,0,0.3], [0.2,0.1,0.6,0.1,0]])
+#%% Without F
+realocation_probs = np.array([[0,0.05,0.1,0.05,0.8], [0.2,0,0.5,0.15,0.15], [0.3,0.2,0,0.2,0.3], 
+                              [0.35,0.3,0.05,0,0.3], [0.2,0.1,0.6,0.1,0]])
+
 parameters = np.array([[55,14.5,2.9,7],[40,11.0,4.0,5],[30,8.0,4.5,2],[20,6.5,1.4,10],[20,5.0,3.9,5]])
 
-rejected = beds_simulation(realocation_probs, parameters, n_patients=1000)
-
-print(rejected)
-
-#%%
+#%% With F
 
 realocation_probs = np.array([[0,0.05,0.1,0.05,0.8, 0], [0.2,0,0.5,0.15,0.15,0], [0.3,0.2,0,0.2,0.3,0],
                               [0.35,0.3,0.05,0,0.3,0], [0.2,0.1,0.6,0.1,0,0],[0.2,0.2,0.2,0.2,0.2,0]])
-parameters = np.array([[55,14.5,2.9,7],[40,11.0,4.0,5],[30,8.0,4.5,2],[20,6.5,1.4,10],[20,5.0,3.9,5], [20, 13.0, 2.2, 4]])
 
-rejected = beds_simulation(realocation_probs, parameters, n_patients=1000)
+parameters = np.array([[55,14.5,2.9,7],[40,11.0,4.0,5],[30,8.0,4.5,2],[20,6.5,1.4,10],[20,5.0,3.9,5], [20, 13.0, 2.2, 9]])
 
-print(rejected)
+#%%
+
+total_iters = 100
+n_patients = 5000
+urgency_weight = True
+ward_distribution = "exp" ## "exp" or "lognormal"
+
+accepted_array = np.zeros((total_iters, parameters.shape[0]))
+relocated_array = np.zeros((total_iters, parameters.shape[0]))
+rejected_array = np.zeros((total_iters, parameters.shape[0]))
+patient_types_count_array = np.zeros((total_iters, parameters.shape[0]))
+penalty_array = np.zeros(total_iters)
+for n_iter in range(total_iters):
+    accepted, relocated, rejected, patient_types_count, penalty = beds_simulation(realocation_probs, parameters, ward_distribution = ward_distribution,
+                                                                                  n_patients=n_patients, urgency_weight=urgency_weight)
+
+    accepted_array[n_iter] = accepted
+    relocated_array[n_iter] = relocated
+    rejected_array[n_iter] = rejected
+    patient_types_count_array[n_iter] = patient_types_count
+    penalty_array[n_iter] = penalty
+#%%
+accepted_percentage = accepted_array/patient_types_count_array
+relocated_percentage = relocated_array/patient_types_count_array
+rejected_percentage = rejected_array/patient_types_count_array
+
+mean_accepted = np.mean(accepted_percentage,axis=0) ## percentage of accepted of each type
+mean_relocated = np.mean(relocated_percentage,axis=0) ## percentage of relocated of each type
+mean_rejected = np.mean(rejected_percentage,axis=0) ## percentage of rejected of each type
+
+mean_penalty = np.mean(penalty_array)
+
+print(mean_rejected)
+print(mean_penalty)
